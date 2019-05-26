7d59c41a3faf922548fbfb10fc2641a3a1648a16
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 19:24:08 -0400

    inicio de proyecto con socket.io

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-/**
- * 
- * ***************
- *  Comience aqui
- * ***************
- * 
- */
+const server = require("http").Server();
+const io = require("socket.io")(server);
+
+const port = require("./config").SERVER_PORT;
+
+const banner = `
+***********************
+    Basic Node.js course
+        course project
+    tic tac toe Server
+***********************
+Status: Online
+Listening on port:${port}
+`;
+
+io.on("connection", socket => {
+  socket.on("register", user => {
+    console.info(`User registed: ${user.name}`);
+  });
+});
+
+server.listen(port, () => {
+  console.info(banner);
+});
