69287f21b7209564b7bab1268447bbefdcca7b39
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 13:20:55 -0400

    async y await

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+//call.greet("Gabriel");
+async function callWithPromise() {
+  const fullname = await call.withPromise("Gabriel", "Ricelis");
+  console.log(fullname);
+}
+
+callWithPromise();
