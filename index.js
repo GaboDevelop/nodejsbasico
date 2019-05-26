23260a2f1774d91993e8959c8886a9893a2f9361
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 12:23:31 -0400

    require module export

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-/**
- * *************
- * Comience aqui
- * *************
- */
+const math = require("./math");
+const greet = require("./greetings");
+const hello = require("./greetings/hello");
+
+console.log(math.add(4, 5));
+console.log(math.add(4, 5));
+console.log(math.add(4, 5));
+console.log(math.add(4, 5));
+
+console.log(greet.greet("Gabriel"));
+console.log(hello.sayHello("Jesus"));
