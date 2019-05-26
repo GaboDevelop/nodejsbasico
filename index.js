b446b6491e4a4fe86107bf94e28090b9a2f0396b
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 14:47:12 -0400

    manejo de eventos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const greet = require("./src/greet");
+
+//greet.emit("clap");
+//greet.emit("shout", "VIVA LO QUE SEA");
+greet.emit("call", "Gabriel", name => {
+  console.log(`estamos llamando a ${name}`);
+});
