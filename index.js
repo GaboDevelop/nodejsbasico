bf0526ca3756e77e33b40ab5d327a400bcea24aa
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 13:05:07 -0400

    callback y promesas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call");
+
+//console.log(call.sync("Gabriel"));
+
+//call.withCallback("Gabriel", call.sync);
+
+call.withPromise("Gabriel").then(name => console.log(name));
--- src/call.js
+++ src/call.js
@@ -1,17 +1,18 @@
 module.exports = {
-    sync: function(nombre) {
-        return "Hola ${nombre}! Bienvenido a Node.js";
-    },
-    withCallback: function(nombre, cb) {
-        setTimeout(()=>{
-            cb(nombre)
-        }, 5000);
-    },
-    withPromise: function(nombre) {
-        return new Promise((resolve, reject)=>{
-            setTimeout(()=>{
-                resolve(nombre);
-            }, 5000); 
-        })
-    }
-}
+  sync: function(nombre) {
+    return `Hola ${nombre}! Bienvenido a Node.js`;
+  },
+  withCallback: function(nombre, cb) {
+    setTimeout(() => {
+      console.log(cb(nombre));
+    }, 5000);
+  },
+  withPromise: function(nombre) {
+    return new Promise((resolve, reject) => {
+      console.log("esperando promesa");
+      setTimeout(() => {
+        resolve(nombre);
+      }, 5000);
+    });
+  }
+};
