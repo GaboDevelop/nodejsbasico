3016c2bc9e482501be3f0acf66a634c87d041161
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 15:01:59 -0400

    cracion de servidor basico con node

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  fs.readFile("./resources/my_page.html", (err, data) => {
+    if (err) {
+      console.err(err);
+      return;
+    }
+    res.end(data);
+  });
+});
+
+console.log("iniciando servidor...");
+server.listen(3000);
