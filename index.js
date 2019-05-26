e3eb7d033b82a733727dee5871ac2b9ac45eed1d
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 12:46:30 -0400

    fundamentos de asincronismo con settimeout

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const add = require("./src/addition");
+const sin = require("./src/intensive");
+
+/*add.syncSum(1, 2);
+add.asyncSum(3, 3);*/
+
+//sin.simulateSync();
+sin.simulateAsync();
