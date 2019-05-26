002da3d3234beb134d64a2752bcb53d5fb8789dc
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 16:04:36 -0400

    creando primer servidor express

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Estoy en la ruta HOME");
+});
+
+app.get("/app", (req, res) => {
+  res.send("Estoy en la ruta /app");
+});
+
+app.get("*", (req, res) => {
+  res.send("Estoy en la ruta 404");
+});
+
+app.listen(3000, () => {
+  console.log("El servidor express esta en escucha en el puerto 3000...");
+});
