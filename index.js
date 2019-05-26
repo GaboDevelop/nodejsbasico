eafc0af26e30a811edc7d2d55662fa8838b7a678
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 13:41:48 -0400

    lectura y escritura de archivos

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const fs = require("fs");
+const ops = require("./src/fileops");
+
+const readFile = path => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (err, list) => {
+      if (err) reject(err);
+
+      resolve(list.split("\n"));
+    });
+  });
+};
+
+const writeFile = (path, value) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, value, (err, list) => {
+      if (err) reject(err);
+      resolve(true);
+    });
+  });
+};
+
+const main = function() {
+  readFile("./resources/number.txt")
+    .then(async res => {
+      const newNumbers = ops.incrementValues(res.map(n => Number(n)));
+      const save = await writeFile("./resources/numbernew.txt", newNumbers);
+      console.log(save);
+    })
+    .catch(err => console.log(err));
+};
+
+main();
