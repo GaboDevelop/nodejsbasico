76b6a3e18587fab0ef00409fa855e1b30f2047bf
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 14:04:57 -0400

    creando subprocesos con comandos

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+//ejecutar npm i
+const cp = require("child_process");
+
+function execCommand(command) {
+  cp.exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.log(`Error: ${err}`);
+      return;
+    }
+    if (stdout) console.log(`standar out:\n${stdout} `);
+    if (stderr) console.log(`standar err:\n${stderr} `);
+  });
+}
+
+//execCommand("ls");
+
+execCommand("node ./src/script --base=5");
