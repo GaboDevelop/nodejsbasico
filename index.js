64eb8f582ef60404ab5b9b8fe9062d397eddf7d8
GaboDeveloper <gabriel.enr1998@gmail.com>
2019-05-26 14:35:15 -0400

    manejo de errores con node

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const errors = require("./src/errors");
+const handle = require("./src/handling");
+
+/*try {
+  errors.standardErr.range();
+} catch (err) {
+  console.log(`Ha ocurrido un error ${err}`);
+}*/
+
+handle.errorFirstCallbackWrong();
